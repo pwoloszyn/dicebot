2e0e407f6a92f55db1bb5e4d02b3692f108ca4ee
Kevin <Kevin.M.Amaral@gmail.com>
2020-07-09 19:29:52 -0400

    Cleaned up unused import.

--- src/main/java/bananaland/dicebot/bot/Bot.java
+++ src/main/java/bananaland/dicebot/bot/Bot.java
@@ -5,8 +5,6 @@ import net.dv8tion.jda.api.entities.MessageChannel;
 import net.dv8tion.jda.api.events.message.MessageReceivedEvent;
 import net.dv8tion.jda.api.hooks.ListenerAdapter;
 
-import javax.annotation.Nonnull;
-
 public class Bot extends ListenerAdapter {
 
     @Override
