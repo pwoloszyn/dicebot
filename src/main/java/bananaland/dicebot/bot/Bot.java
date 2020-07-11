e2fd43286f49237a7735d879b6c284e62e2d87c7
mrcoffee <piotrwoloszyn00gmail.com>
2020-07-11 16:11:53 -0400

    Added a comment to Bot

--- src/main/java/bananaland/dicebot/bot/Bot.java
+++ src/main/java/bananaland/dicebot/bot/Bot.java
@@ -6,6 +6,8 @@ import net.dv8tion.jda.api.events.message.MessageReceivedEvent;
 import net.dv8tion.jda.api.hooks.ListenerAdapter;
 
 public class Bot extends ListenerAdapter {
+	
+	//Some cahnges will happen
 
     @Override
     public void onMessageReceived(MessageReceivedEvent event) {
