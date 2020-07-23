863312fa1c8e607c0a07456be7a2145cbd940e64
mrcoffee <piotrwoloszyn00gmail.com>
2020-07-23 19:46:30 -0400

    Fixed the joke.

--- src/main/java/bananaland/dicebot/bot/Joke.java
+++ src/main/java/bananaland/dicebot/bot/Joke.java
@@ -2,6 +2,6 @@ package bananaland.dicebot.bot;
 
 public class Joke {
     static {
-        System.out.println("You bootloaded this shit?");
+        System.out.println("Why did the chicken cross the road? To git to the otherside.");
     }
 }
