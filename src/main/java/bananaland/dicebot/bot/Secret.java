2ef14ff7b7443135654cc69278338d8cb38be6df
mrcoffee <piotrwoloszyn00gmail.com>
2020-10-21 00:51:00 -0400

    Regenerated the token again

--- src/main/java/bananaland/dicebot/bot/Secret.java
+++ src/main/java/bananaland/dicebot/bot/Secret.java
@@ -2,5 +2,5 @@ package bananaland.dicebot.bot;
 
 public final class Secret {
     //API token for the DiceBot
-    public static final String TOKEN = "nO_XalcJ7_Q4UIg_t-KN9b4osJ1WlPeS";
+    public static final String TOKEN = "NzQxNjY0NzIzNTgxMjcyMTAz.Xy63GQ.S0naQlypLwR9X2uK0Tvp3P3sBq4";
 }
